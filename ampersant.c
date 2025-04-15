908c7d73ca49c2a5ecb77abf08764e54cd9cc2ac
elshadyusifov <e.yusifov335@gmail.com>
2025-04-15 21:37:05 +0400

    create c ampersant file

---
+++ ampersant.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+ int i;
+ 
+ printf("xahis edirik reqem daxil edin\n");
+ scanf("%d", &i);
+ 
+ if(i >= 10 && i <= 20 && i%2 == 0){
+ printf("Eded verilen shertleri odeyir! Tebrik edirik!");
+ }
+ else
+ {
+     printf("yalnisdir");
+     }
+    return 0;
+}
