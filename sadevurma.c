1681d1c86ecacfc054a18507f495997ccf951d5f
babakyusifov <babek@div.edu.az>
2025-04-18 21:51:53 +0400

    sade vurma

---
+++ sadevurma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int a;
+  int b;
+  int result = a * b;
+  
+   printf("Eded daxil edin\n");
+      scanf(" %d %d", &a, &b); 
+    printf("%d", a*b);
+  
+  return 0;
+}
